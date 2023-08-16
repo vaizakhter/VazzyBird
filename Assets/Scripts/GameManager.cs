f0d57530f13d2a380519e9f04f9179523ccec1ed
Vaiz Akhter Ansari <Vaiz@mcontent.net>
2023-08-16 10:17:25 +0530

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private Player player;
+    private Spawner spawner;
+
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
+    public GameObject gameStart;
+
+    public int score { get; private set; }
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+
+        player = FindObjectOfType<Player>();
+        spawner = FindObjectOfType<Spawner>();
+
+        Pause();
+    }
+
+    public void Play()
+    {
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+        gameStart.SetActive(false);
+
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+
+        for (int i = 0; i < pipes.Length; i++) {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void GameOver()
+    {
+        playButton.SetActive(true);
+        gameOver.SetActive(true);
+
+        Pause();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+}
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+
+    public float animationSpeed = 1f;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+
+    }
+
+    public void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(animationSpeed * Time.deltaTime, 0);
+    }
+
+
+}
